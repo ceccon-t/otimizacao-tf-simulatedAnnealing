8ec0b988b387c9190d5b693127ecd3a1b932b354
Tiago Ceccon <ceccon.t@hotmail.com>
2021-10-26 17:12:55 -0300

    SA - Adiciona algumas funcoes auxiliares para calcular graus e refatoracoes

--- simulatedAnnealing/simulatedAnnealing.py
+++ simulatedAnnealing/simulatedAnnealing.py
@@ -1,26 +1,24 @@
 import os
 import math 
 
-e = math.e
-
 
 def vtoi(v: int) -> int:
     """
-        Converte de numero de vertice (1-indexed) para seu indice na matriz de adjacencia (0-indexed)
+        Converts from 1-indexed vertex 'name' to 0-indexed index on adjacency matrix
     """
     return v - 1
 
 
 def itov(i: int) -> int:
     """
-        Converte de numero do indice na matriz de adjacencia (0-indexed) para o numero do vertice (1-indexed)
+        Converts from 0-indexed index on adjacenct matrix to 1-indexed vertex 'name'
     """
     return i + 1
 
 
 def parse_int_pair(s: str) -> 'tuple[int]':
     """
-        Dada uma string no formato "A B", com A e B numeros inteiros retorna tupla com inteiros A e B nessa ordem
+        Given a string with format "A B", with A and B being integers, returns tuple with ints A and B, in this order
     """
     a, b = s.split(" ")
     a = int(a)
@@ -44,9 +42,38 @@ def remove_edge(M, u, v):
 
 # TODO: funcao para avaliar funcao objetivo
 
-# TODO: (talvez) funcao para calcular grau de um vertice no grafo original
 
-# TODO: funcao para calcular grau de um vertice em uma solucao especifica
+def degree_on_original(v: int) -> int:
+    """
+        Calculate degree of vertex 'v' on original graph, probably more useful when generating original solution
+    """
+    global original_graph  # Vou pensar em algo melhor do que isso depois
+    i = vtoi(v)
+    return sum(original_graph[i])
+
+
+def degree_on_solution(v: int, s: 'set[int]') -> int:
+    """
+        Calculate degree of vertex 'v' on solution 's'
+    """
+    global original_graph  # Vou pensar em algo melhor do que isso depois
+    i = vtoi(v)
+    degree = 0
+    for u in s:
+        j = vtoi(u)
+        degree += original_graph[i][j]
+    return degree
+
+
+def is_valid_solution_full(s: 'set[int]') -> bool:
+    """
+        Checks if solution is valid by looking on original graph (not differential)
+    """
+    for v in s:
+        deg = degree_on_solution(v, s)
+        if deg % 2 != 0:
+            return False
+    return True 
 
 
 # Parametros
@@ -61,6 +88,7 @@ cooling_rate = 0.8          # r     : idealmente entre [0.8, 0.99]
 
 
 def boltzmann(x: float, temperature: float) -> float:
+    e = math.e
     exponent = - x / temperature
     return e ** exponent
 
@@ -95,16 +123,24 @@ with open(filepath, 'r') as instancia:
         j = vtoi(v2)
         add_edge(original_graph, i, j)
 
-    for i in range(total_vertices):
-        print(f'Vertex {itov(i)}')
+    # Some prints to test initialization of matrix and helper functions
+    all_vertices = set([i for i in range(total_vertices)])
+    # print(all_vertices)
+    simple_solution_for_small = set([1,4,5])
+    # for i in range(total_vertices):
+    #     print(f'Vertice {itov(i)} tem grau {degree_on_original(itov(i))} no grafo original')
+    #     print(f'Vertice {itov(i)} tem grau {degree_on_solution(itov(i), all_vertices)} em pseudo-solucao usando todos os vertices')
+    #     print(f'Vertice {itov(i)} tem grau {degree_on_solution(itov(i), simple_solution_for_small)} em solucao com [1,4,5]')
+    print(f'Is {all_vertices} valid as a solution? : {is_valid_solution_full(all_vertices)}')
+    print(f'Is {simple_solution_for_small} valid as a solution? : {is_valid_solution_full(simple_solution_for_small)}')
 
     
 
 
-print('Matriz com dados originais da instancia:')
+print('Matrix with original instance data:')
 print(original_graph)
 
-print(f'Total de vertices na instancia: {total_vertices}')
-print(f'Total de arestas na instance: {total_edges}')
+print(f'Total number of verticies on instance: {total_vertices}')
+print(f'Total number of edges on instance: {total_edges}')
 
 print('Cest fini.')
